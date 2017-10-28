bb0fe9fb9e2b1131b51108d35e6a645d0a2c2d68
beemargarida <bibibaldaia@hotmail.com>
2017-10-28 10:53:08 +0100

    Ups

--- proj01/appLayer.c
+++ proj01/appLayer.c
@@ -11,19 +11,19 @@
 
 int appTransmiter(char* fileName){//
 	printf("%s\n %d\n",fileName,strlen(fileName));
-	int fdData = open(fileName,O_RDONLY);
-	if(fdData == -1) {
-		printf("Nao conseguio abrir ficheiro de fdData\n");
+	int fdFile = open(fileName,O_RDONLY);
+	if(fdFile == -1) {
+		printf("Can't open the file.\n");
 		return -1;
 	}
-	int fd = llopen(0,TRANSMITER);
+	int fd = llopen(0, TRANSMITER);
 	if(fd == -1) {
-		printf("Nao conseguio abrir canal de comunicacao.\n");
+		printf("Can't open the serial port.\n");
 		return -1;
 	}
 	struct stat st;
 	if (stat(fileName, &st) != 0) {
-		printf("Erro no stat\n");
+		printf("Error on stat\n");
 		return -1;
 	}
 	size_t fileSize = st.st_size;
@@ -37,102 +37,105 @@ int appTransmiter(char* fileName){//
 	controlPacket[0] = 0x02; //C
 	controlPacket[1] = 0; //T: 0 = comprimento do ficheiro
 	controlPacket[2] = sizeof(fileSize); //L
-	size_t * locationSize = (size_t* )(&controlPacket[3]);
-	*locationSize = fileSize;
-	controlPacket[3+sizeof(fileSize)] = 1; //T: 1 = nome do ficheiro
-	controlPacket[4+sizeof(fileSize)] = strlen(fileName); //L
-	strcpy(&controlPacket[5+sizeof(fileSize)],fileName);
+	size_t * fileSizeOnArray = (size_t* )(&controlPacket[3]);
+	*fileSizeOnArray = fileSize;
+	controlPacket[3 + sizeof(fileSize)] = 1; //T: 1 = nome do ficheiro
+	controlPacket[4 + sizeof(fileSize)] = strlen(fileName); //L
+	strcpy(&controlPacket[5 + sizeof(fileSize)], fileName);
 
 
-	if(llwrite(fd,controlPacket,controlPacketSize) <= 0){
-		printf("Erro no llwrite\n");
+	if(llwrite(fd, controlPacket, controlPacketSize) <= 0){
+		printf("Error on llwrite\n");
 		return -1;
 	}
-	//Enviar inicial--^
+
 	unsigned char n = 0;
-	int pacotefdDataSize=/*Numero0*/ 255+4;
-	unsigned char* pacotefdData = realloc(NULL,pacotefdDataSize);
+	int nDataBytes = 256;
+	int dataPacketSize= nDataBytes + 4;
+	unsigned char* fdDataPacket = realloc(NULL,dataPacketSize);
 	int size = -1;
-	while(0<(size = read(fdData, &(pacotefdData[4]),255/*Numero*/))){
-		//Enviar fdData
-		pacotefdData[0] = 1;//C
-		pacotefdData[1] = n;//N
-		n++;
-		pacotefdData[2]= 0;//LL //TODO ProvavellyWrong size/255(6)
-		pacotefdData[3]= size;///*Numero*/ size % 256(5)
-        int i=0;
-        for(i=0;i<size+4;i++){
-          //  printf("pacotefdData[%d] = %d\n",i,pacotefdData[i]);//C
-        }
-		if(!llwrite(fd,pacotefdData,size+4)){
-			printf("Erro no llwrite\n");
+	while((size = read(fdFile, &(fdDataPacket[4]), nDataBytes)) < 0){
+		//Enviar fdFile
+		fdDataPacket[0] = 0;//C
+		fdDataPacket[1] = n;//N
+		n = (n + 1) % 256;
+		fdDataPacket[2]= size / 256;//LL
+		fdDataPacket[3]= size % 256;///*Numero*/ size % 256(5)
+
+		if(!llwrite(fd, fdDataPacket, size + 4)) {
+			printf("Error on llwrite\n");
 			return -1;
 		}
 	}
 	//Enviar final
-	controlPacket[0]=3;
-	if(!llwrite(fd,controlPacket,controlPacketSize)){
-		printf("Erro no llwrite\n");
+	controlPacket[0] = 3;
+	if(!llwrite(fd, controlPacket, controlPacketSize)) {
+		printf("Error on llwrite\n");
 		return -1;
 	}
+
 	llclose(fd);
-    close(fdData);
+  close(fdFile);
 	return 0;
 }
-int appReceiver(){//
-	//printf("\nfez open\n\n");
-	int fd = llopen(0,RECEIVER);
+int appReceiver() {
+	int fd = llopen(0, RECEIVER);
 	if(fd == -1){
-		printf("Nao conseguiu abrir canal de comunicacao.\n");
-		return -1;
-	}
-	//printf("saiu do llopen\n");
-	unsigned char* pacotefdData= NULL;
-	pacotefdData = realloc(pacotefdData,1000);//Alter
-	int size=0;
-	unsigned char n = 0;
-	int controleSize = llread(fd, pacotefdData); //Leu pacote de controle
-
-	char* fileName = malloc(controleSize);
-  /*fileName tentar ver problema*/
-	int i = 0;
-
-	for(i = 0; i < controleSize; i++){
-		printf("%X\n",pacotefdData[i] );
-	}
-	/** Type
-	 * 0 – tamanho do ficheiro,
-	 * 1 – nome doficheiro
-	*/
-	if (pacotefdData[1] == 0) {
-		i = 3 + pacotefdData[2] + 2;
-	} else {
-		i = 3;
-		controleSize = pacotefdData[2] + 3;
-	}
-	int k = 0;
-	for ( k = 0; i < controleSize; i++, k++) {
-		fileName[k]=pacotefdData[i];
-	}
-printf("%s\n",fileName );
-	int fdData = open(fileName,O_CREAT|O_RDWR|O_TRUNC,0666);
-	if(fdData == -1){
-		printf("Nao conseguiu abrir ficheiro de fdData\n");
+		printf("Couldn't open the serial port.\n");
 		return -1;
 	}
 
-	while(0<(size = llread(fd, pacotefdData))) {//NOT FINAL
-		if((pacotefdData[0] == 1) && (pacotefdData[1] == n)){
-			n++;
-			int sizeWrite = 4;
-			int stillNeedToWrite=(size - sizeWrite);
-			while((stillNeedToWrite=((size - sizeWrite)))> 0){
-				sizeWrite += write(fdData, pacotefdData + sizeWrite, stillNeedToWrite);
+	unsigned char* controlPacket = NULL;
+	controlPacket = realloc(controlPacket, 1000);
+	int size = 0;
+	 n = 0;
+	int controlPacketSize = llread(fd, controlPacket); //Leu pacote de controle
+
+	if(controlPacketSize <= 0) {
+		printf("Error on llread.\n");
+		return -1;
+	}
+
+	int i = 0, j = 0;
+	unsigned int* fileSize = NULL;
+	char* fileName = malloc(controlPacketSize);
+
+	for(; i < controlPacketSize; i++) {
+		if(controlPacket[i] == 0) { //file size
+			memcpy(fileSize, controlPacket[i + 2], controlPacket[i + 1]);
+			i += 1 + controlPacket[i + 1];
+		} else if(controlPacket[i] == 1) { //file size
+			int fileNameSize = controlPacket[i + 1];
+			i += 2;
+			for (; j < fileNameSize; j++) {
+				fileName[j] = controlPacket[i];
+			}
 		}
-		}else break;
+	}
+
+  printf("%s\n",fileName );
+
+	int fdFile = open(fileName, O_CREAT | O_RDWR | O_TRUNC, 0666);
+	if(fdFile == -1){
+		printf("Couldn't open the file.\n");
+		return -1;
+	}
+
+	unsigned char* dataPacket = NULL;
+	dataPacket = realloc(dataPacket, *fileSize);
+
+	while((size = llread(fd, dataPacket)) > 0) {//NOT FINAL
+		if((dataPacket[0] == 0) && (dataPacket[1] == n)) {
+			n = (n + 1) % 256;
+			int sizeWrite = 4;
+			int writeToFile = (size - sizeWrite);
+			while((writeToFile = (size - sizeWrite)) > 0) {
+				sizeWrite += write(fdFile, fdDataPacket + sizeWrite, stillNeedToWrite);
+		  }
+		} else break;
 	}
     llclose(fd);
-    close(fdData);
+    close(fdFile);
 	return -1;
 }
 
